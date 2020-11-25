e348fd2f6bd2deb95a2c14c6fa44774c05e24c68
fmujkic2 <fmujkic2@etf.unsa.ba>
2020-11-25 17:57:31 +0100

    Zavrsen prvi zadatak

--- zadatak/iscrtaj.js
+++ zadatak/iscrtaj.js
@@ -1,11 +1,11 @@
-function iscrtajRaspored(div,dani,satPocetak,satKraj) {
+function iscrtajRaspored(div, dani, satPocetak, satKraj) {
 
-    if(satKraj<=satPocetak || satPocetak<0 || satPocetak>24 || satKraj<0 || satKraj>24){
-      div.appendChild(document.createTextNode("Greska"));  
-      return;
+    if (satKraj <= satPocetak || satPocetak < 0 || satPocetak > 24 || satKraj < 0 || satKraj > 24) {
+        div.appendChild(document.createTextNode("Greska"));
+        return;
     }
-    
-    r=satKraj-satPocetak;
+
+    r = satKraj - satPocetak;
 
     var myTableDiv = div;
 
@@ -20,33 +20,41 @@ function iscrtajRaspored(div,dani,satPocetak,satKraj) {
     tableBody.appendChild(tr);
     for (var j = 0; j <= r; j++) {
         var th = document.createElement('TH');
-        if(j>0){
+        if (j > 0) {
             th.setAttribute("colspan", "2");
         }
-        
-        if(j%2==1){
-            th.appendChild(document.createTextNode((j+satPocetak-1)+":00"));
+
+    var h=j + satPocetak - 1;
+
+        if ((h<13 && h % 2 == 0) || (h>13 && h % 2 == 1 )){
+            th.appendChild(document.createTextNode((h) + ":00"));
+            th.setAttribute("id", satPocetak);
         }
         tr.appendChild(th);
     }
 
     for (var i = 0; i < dani.length; i++) {
         var tr = document.createElement('TR');
+        tr.setAttribute("id", dani[i])
         tableBody.appendChild(tr);
 
-        for (var j = 0; j < r*2+1; j++) {
+        for (var j = 0; j < r * 2 + 1; j++) {
             var td = document.createElement('TD');
 
             if (j == 0) {
                 var td = document.createElement('TD');
                 td.setAttribute("class", "posebna");
+                
                 td.appendChild(document.createTextNode(dani[i]));
             }
 
             else if (j % 2 == 1) {
                 td.style.borderLeft = '1px solid black';
             }
-            // td.appendChild(document.createTextNode("Cell " + i + "," + j));
+
+             if(j==1){
+                td.setAttribute("class", satPocetak);
+             }
             tr.appendChild(td);
         }
     }
@@ -54,23 +62,41 @@ function iscrtajRaspored(div,dani,satPocetak,satKraj) {
 }
 
 
-function dodajAktivnost(raspored, naziv, tip, vrijemePocetak, vrijemeKraj,dan){
+function dodajAktivnost(raspored, naziv, tip, vrijemePocetak, vrijemeKraj, dan) {
 
-    if(raspored==null || vrijemeKraj<=vrijemePocetak || vrijemePocetak<0 || vrijemePocetak>24 || vrijemeKraj<0 || vrijemeKraj>24)alert("Greska");
-
-
-    var row = raspored.getElementsByTagName("tr")[1];
-
-    for(var i=0;i<5;i++)
-    row.deleteCell(3);
-
-    
-   var cell=row.getElementsByTagName("td")[3];
+    if (raspored == null || vrijemeKraj <= vrijemePocetak || vrijemePocetak < 0 || vrijemePocetak > 24 || vrijemeKraj < 0 || vrijemeKraj > 24) alert("Greska");
 
 
-    cell.colSpan = "6" ;
+    var satPocetak = document.getElementsByTagName("th")[1].id;
 
-    cell.appendChild(document.createTextNode( naziv + tip));  
+    var row = document.getElementById(dan);
+
+    var p = (vrijemeKraj-vrijemePocetak)*2;
+
+   
+    var q=(vrijemePocetak-satPocetak)*2+1
+
+    for(var c = 0; c <= q; c++){
+
+       if( row.getElementsByTagName("td")[c].colSpan>1){
+       q-=row.getElementsByTagName("td")[c].colSpan-1;
+       }
+       if(c>q)alert("Sranje");
+    }
+
+
+   
+
+    for (var i = 0; i < p-1; i++)
+        row.deleteCell(q);
+
+
+    var cell = row.getElementsByTagName("td")[q];
+
+
+    cell.colSpan = p;
+
+    cell.appendChild(document.createTextNode(naziv + tip));
 
     cell.setAttribute("class", "popunjeno");
 
--- zadatak/spirala2rasporedi.js
+++ zadatak/spirala2rasporedi.js
@@ -1,9 +1,9 @@
 let okvir=document.getElementById('okvir');
-iscrtajRaspored(okvir,['Ponedjeljak','Utorak','Srijeda','Cetvrtak','Petak'],8,21);
+iscrtajRaspored(okvir,['Ponedjeljak','Utorak','Srijeda','Cetvrtak','Petak'],0,24);
 dodajAktivnost(okvir,'WT','predavanje',9,12,'Ponedjeljak');
-/*dodajAktivnost(okvir,”WT”,”vježbe”,12,13.5,”Ponedjeljak”);
-dodajAktivnost(okvir,”RMA”,”predavanje”,14,17,”Ponedjeljak”);
-dodajAktivnost(okvir,”RMA”,”vježbe”,12.5,14,”Utorak”);
-dodajAktivnost(okvir,”DM”,”tutorijal”,14,16,”Utorak”);
-dodajAktivnost(okvir,”DM”,”predavanje”,16,19,”Utorak”);
-dodajAktivnost(okvir,”OI”,”predavanje”,12,15,”Ponedjeljak”);*/
+dodajAktivnost(okvir,'WT','vježbe',12,13.5,'Ponedjeljak');
+dodajAktivnost(okvir,'RMA','predavanje',14,17,'Ponedjeljak');
+dodajAktivnost(okvir,'RMA','vježbe',12.5,14,'Petak');
+dodajAktivnost(okvir,'DM','tutorijal',14,16,'Utorak');
+dodajAktivnost(okvir,'DM','predavanje',16,19,'Utorak');
+//dodajAktivnost(okvir,'OI','predavanje',12,15,'Ponedjeljak');
