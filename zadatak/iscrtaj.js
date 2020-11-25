ed3e71baefca87f1502b34602b2378b6658df61e
fmujkic2 <fmujkic2@etf.unsa.ba>
2020-11-25 02:27:36 +0100

    zavrsena prva fja

--- zadatak/iscrtaj.js
+++ zadatak/iscrtaj.js
@@ -1,36 +1,39 @@
 function addTable(div,dani,satPocetak,satKraj) {
 
+    if(satKraj<=satPocetak || satPocetak<0 || satPocetak>24 || satKraj<0 || satKraj>24){
+      div.appendChild(document.createTextNode("Greska"));  
+      return;
+    }
     
+    r=satKraj-satPocetak;
+
     var myTableDiv = div;
 
     var table = document.createElement('TABLE');
-
-    var t=dani.length;
-
     var tableBody = document.createElement('TBODY');
-
     table.appendChild(tableBody);
+
 
     var tr = document.createElement('TR');
     tr.setAttribute("class", "naslov");
     tableBody.appendChild(tr);
-    for (var j = 0; j < 14; j++) {
+    for (var j = 0; j <= r; j++) {
         var th = document.createElement('TH');
         if(j>0){
             th.setAttribute("colspan", "2");
         }
         
         if(j%2==1){
-            th.appendChild(document.createTextNode((j+7)+":00"));
+            th.appendChild(document.createTextNode((j+satPocetak-1)+":00"));
         }
         tr.appendChild(th);
     }
 
-    for (var i = 0; i < t; i++) {
+    for (var i = 0; i < dani.length; i++) {
         var tr = document.createElement('TR');
         tableBody.appendChild(tr);
 
-        for (var j = 0; j < 27; j++) {
+        for (var j = 0; j < r*2+1; j++) {
             var td = document.createElement('TD');
 
             if (j == 0) {
